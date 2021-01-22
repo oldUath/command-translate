dbcd8677c069381d8707c02107fbc89459aad8d1
oldUath <haiyangdong666@gmail.com>
2021-01-22 11:19:28 +0800

    配置命令行的版本信息

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,10 @@
 //存放commander命令行
-
 const { Command } = require('commander');
+const program = new Command();
+
+//控制命令行的版本名称和参数
+program.version('0.0.1')
+    .name('translate')
+    .usage('<english>');
+
+program.parse(process.argv);
