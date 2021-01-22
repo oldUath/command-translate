2adf98b7432a9207005f4e4c4ea29f89418d0b9a
oldUath <haiyangdong666@gmail.com>
2021-01-22 22:24:16 +0800

    调用接口测试

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,17 @@
 //存放commander命令行
+import {translate} from './main';
+
 const { Command } = require('commander');
 const program = new Command();
 
 //控制命令行的版本名称和参数
 program.version('0.0.1')
     .name('translate')
-    .usage('<english>');
+    .usage('<english>')
+    .arguments('<english>')
+    .action(function (english){
+        translate(english)
+    })
+;
 
 program.parse(process.argv);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log('hi');
+import * as https from 'https';
+
+export const translate = (word)=>{
+
+    const options = {
+        hostname: 'www.baidu.com',
+        port: 443,
+        path: '/',
+        method: 'GET'
+    };
+
+    const req = https.request(options, (res) => {
+        console.log('状态码:', res.statusCode);
+        console.log('请求头:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    req.end();
+}
