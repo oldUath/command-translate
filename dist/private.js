43a0f8a97fe067fd8dd6eb90b9e0151fdb178270
oldUath <haiyangdong666@gmail.com>
2021-01-26 18:13:13 +0800

    删除

--- dist/private.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appSecert = exports.appId = void 0;
-exports.appId = '20210122000678235';
-exports.appSecert = 'ceTGqgwlex2qyg8pWVMQ';
